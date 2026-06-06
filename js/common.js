4f3c14fe2ed3e596f2d02637c91e1bd8d2bc787c
Viktoria Zaharova <zaharova181114@gmail.com>
2026-06-07 00:36:21 +0300

    start

--- js/common.js
+++ js/common.js
@@ -1,2 +1,18 @@
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
+$(function() {
+    const $track = $('.news-ticker__track');
+    $track.append($track.html());
+});
+
+// animate scroll to section
+$('.go_to').on('click', function(e) {
+    e.preventDefault();
+
+    const target = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(target).offset().top
+    }, 800);
+});
+
